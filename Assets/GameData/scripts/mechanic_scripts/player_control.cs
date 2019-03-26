fd1e3ed661caa6e0133faaeab06b307747c70efc
Longlife Games <po4tajurajkeee@gmail.com>
2019-03-26 23:21:52 +0200

    Update

---
+++ Assets/GameData/scripts/mechanic_scripts/player_control.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax;
+}
+
+public class player_control : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+    private float posY;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        posY = this.gameObject.transform.position.y;
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+
+        Vector3 movement = new Vector3(moveHorizontal, posY, rb.position.z);
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector2
+            (
+                Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+                posY
+            );
+    }
+}
