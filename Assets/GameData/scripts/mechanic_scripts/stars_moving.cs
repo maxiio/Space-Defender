2727e9362de19523d18cbdd75bbc99be4553d5bc
Longlife Games <po4tajurajkeee@gmail.com>
2019-04-04 00:33:36 +0300

    Stars Paralax

---
+++ Assets/GameData/scripts/mechanic_scripts/stars_moving.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stars_moving : MonoBehaviour
+{
+    public float paralax_brakes;
+    void Update()
+    {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        Material mat = mr.material;
+        Vector2 offset = mat.mainTextureOffset;
+        offset.y += Time.deltaTime/ paralax_brakes;
+        mat.mainTextureOffset = offset;
+    }
+}
