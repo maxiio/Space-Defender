c5a8e644c94ec772a85ea2ea2f0f43177adbcf33
Mobot <po4tajurajkeee@gmail.com>
2019-04-07 15:52:13 +0300

    123

---
+++ Assets/Scenes/Physics/Follower.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class Follower : MonoBehaviour
+{
+    public PathCreator pathCreator;
+    public float speed = 5;
+    public EndOfPathInstruction endOfPathInstruction;
+    float distanceTravelled;
+    private Rigidbody2D rb;
+
+
+    public bool continueFollowing = true;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (continueFollowing)
+        {
+            //distanceTravelled += rb.velocity.y* rb.velocity.x* Time.deltaTime;
+            //rb.AddForce(pathCreator.path.GetDirectionAtDistance(distanceTravelled, endOfPathInstruction));
+            //transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+            Debug.DrawRay(transform.position, pathCreator.path.GetDirectionAtDistance(distanceTravelled, endOfPathInstruction),Color.red);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        continueFollowing = false;
+    }
+}
