5d5f4b43facc912fa31f461bfec8d2d3611d2322
Longlife Games <po4tajurajkeee@gmail.com>
2019-04-09 19:05:54 +0300

    123

---
+++ Assets/GameData/scripts/mechanic_scripts/obstacle_activator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacle_activator : MonoBehaviour
+{
+    public GameObject localObstaclesGroup;
+    private void Start()
+    {
+        localObstaclesGroup = transform.GetChild(1).gameObject;
+        localObstaclesGroup.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            localObstaclesGroup.SetActive(true);
+        }
+    }
+}
