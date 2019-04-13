1c190f645853696bec16676df14d2caac5068b97
Mobot <po4tajurajkeee@gmail.com>
2019-04-13 13:29:21 +0300

    Shooting and First Obstacles

--- Assets/Scenes/Physics/pathmoving.cs
+++ Assets/Scenes/Physics/pathmoving.cs
@@ -11,17 +11,19 @@ public class pathmoving : MonoBehaviour
     private void Start()
     {
         pathCreator = this.gameObject.GetComponent<PathCreator>();
+        Debug.Log(pathCreator.transform.position);
+
         for (int i = 0; i < pathCreator.path.vertices.Length; i++)
         {
-            pathCreator.path.vertices[i] = new Vector3(pathCreator.path.vertices[i].x, (this.transform.position.y - pathCreator.path.vertices[i].y), pathCreator.path.vertices[i].z);
+            pathCreator.path.vertices[i] = new Vector3(pathCreator.path.vertices[i].x, transform.parent.position.y + pathCreator.path.vertices[i].y, pathCreator.path.vertices[i].z);
         }
             singletone = GameObject.FindObjectOfType<singletone_script>();
     }
-    private void FixedUpdate()
+    private void Update()
     {
         for (int i=0; i< pathCreator.path.vertices.Length; i++)
         {
-        pathCreator.path.vertices[i] = Vector3.SmoothDamp(pathCreator.path.vertices[i], new Vector3(pathCreator.path.vertices[i].x, pathCreator.path.vertices[i].y - (singletone.playerSpeed / 100f), pathCreator.path.vertices[i].z), ref velocity, 0.1F);
+            pathCreator.path.vertices[i] = Vector3.SmoothDamp(pathCreator.path.vertices[i], new Vector3(pathCreator.path.vertices[i].x, pathCreator.path.vertices[i].y - (singletone.playerSpeed / 100f), pathCreator.path.vertices[i].z), ref velocity, 0.1F);
         }
     }
 }
