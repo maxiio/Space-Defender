cbec6da2a798b13ba1f0d2106c8eef1c75d430b6
Mobot <po4tajurajkeee@gmail.com>
2019-04-26 15:53:33 +0300

    Updated Stars Parallax

--- Assets/GameData/scripts/mechanic_scripts/stars_moving.cs
+++ Assets/GameData/scripts/mechanic_scripts/stars_moving.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class stars_moving : MonoBehaviour
 {
     public float paralax_brakes;
+    public singletone_script singletone;
+    private MeshRenderer mr;
+    private void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+    }
     void Update()
     {
-        MeshRenderer mr = GetComponent<MeshRenderer>();
-        Material mat = mr.material;
-        Vector2 offset = mat.mainTextureOffset;
-        offset.y += Time.deltaTime/ paralax_brakes;
-        mat.mainTextureOffset = offset;
+        if (!singletone.disable_control || singletone.main_menu_canvas.enabled==true)
+        {
+            Material mat = mr.material;
+            Vector2 offset = mat.mainTextureOffset;
+            offset.y += Time.deltaTime / paralax_brakes;
+            mat.mainTextureOffset = offset;
+        }
     }
 }
